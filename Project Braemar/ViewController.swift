bf18b16914e5d17e16d29407cc58206334e5fe88
HamishW08 <16300@braemar.vic.edu.au>
2023-10-30 12:48:04 +1100

    Initial Commit

---
+++ Project Braemar/ViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    @IBOutlet weak var loadingLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Start your loading logic here
+        DispatchQueue.global().async {
+            // Simulate a delay (replace with your actual loading code)
+            Thread.sleep(forTimeInterval: 2.0)
+            
+            // Update UI on the main thread
+            DispatchQueue.main.async {
+                self.loadingLabel.text = "Loading complete!"
+                
+                // You can transition to the main content of your app here
+            }
+        }
+    }
+}
